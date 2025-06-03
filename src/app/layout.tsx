0d269f92cea9dd1d48ecd766d722e2ed60a2006e
KaziMahbuburRahman <Mahbubbengali@gmail.com>
2025-06-03 22:59:35 +0600

    Add Google Tag Manager integration to layout component
    Incorporate Google Tag Manager scripts into the RootLayout component for improved tracking and analytics. This includes both the main script and the noscript fallback for better performance and compliance.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { GeistSans } from "geist/font/sans";
+import Script from "next/script";
 
-import { TRPCReactProvider } from "@/trpc/react";
 import { Toaster } from "@/components/ui/sonner";
-import AnalyticsScript from "@/components/ga-pixel-script/AnalyticsScript";
-import Head from "next/head";
+import { TRPCReactProvider } from "@/trpc/react";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -19,9 +19,33 @@ export default function RootLayout({
           content="Your one-stop shop for premium products. Discover our curated collection of quality items with secure shopping and fast delivery."
         />
         <link rel="icon" href="/favicon.ico" />
-        <AnalyticsScript />
       </Head>
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TXM9N777');
+          `,
+        }}
+      />
+      {/* End Google Tag Manager */}
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TXM9N777"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SessionProvider>
           <TRPCReactProvider>{children}</TRPCReactProvider>
         </SessionProvider>
