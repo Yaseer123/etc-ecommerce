e4a0847335d5f9303f808e3845ca514f7a20aa3d
KaziMahbuburRahman <Mahbubbengali@gmail.com>
2025-06-16 04:51:23 +0600

    Wrap Page component in a div for improved layout structure. This change enhances the overall styling and prepares for future design adjustments.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,9 +4,9 @@ import Navbar from "@/components/admin-components/Navbar";
 
 export default function Page({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="bg-white">
       <Navbar />
       {children}
-    </>
+    </div>
   );
 }
