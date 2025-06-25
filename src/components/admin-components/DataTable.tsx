ae472e765e18f3abce5e6b3f8e35b3e380355aa4
KaziMahbuburRahman <Mahbubbengali@gmail.com>
2025-06-26 02:09:39 +0600

    Update pagination options in DataTable component. Reduced available page sizes from 8 options to 6 for improved clarity and usability.

--- src/components/admin-components/DataTable.tsx
+++ src/components/admin-components/DataTable.tsx
@@ -284,7 +284,7 @@ function BackendPagination({
             value={limit}
             onChange={(e) => onLimitChange?.(Number(e.target.value))}
           >
-            {[10, 20, 30, 40, 50, 100, 200, 300].map((pageSize) => (
+            {[10, 20, 30, 40, 50, 100].map((pageSize) => (
               <option key={pageSize} value={pageSize}>
                 {pageSize}
               </option>
