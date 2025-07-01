61d8e5611e0e49ae3bc0a1eceea32ff969497249
KaziMahbuburRahman <Mahbubbengali@gmail.com>
2025-07-01 22:12:49 +0600

    Add zustand for state management and create layout component for store. Updated package.json and pnpm-lock.yaml to include zustand dependency. Introduced a new layout component that integrates existing UI elements for improved structure in the store section.

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,28 @@
+import { AuroraBackground } from "@/components/shared/AuroraBackground";
+import SlideNavbar from "@/components/shared/SlideNavbar";
+import Footer from "@/components/store-components/Footer";
+import ModalWrapper from "@/components/store-components/Modal/ModalWrapper";
+import WhatsAppWidget from "@/components/WhatsAppWidget";
+import { auth } from "@/server/auth";
+import "@/styles/styles.scss";
+import { HydrateClient } from "@/trpc/server";
+import React from "react";
+
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+
+  return (
+    <HydrateClient>
+      <SlideNavbar>
+        <AuroraBackground>{children}</AuroraBackground>
+      </SlideNavbar>
+      <WhatsAppWidget />
+      <Footer />
+      <ModalWrapper />
+    </HydrateClient>
+  );
+}
