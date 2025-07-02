3b9829be1c38a2829998f46c1939626832d6b5d7
KaziMahbuburRahman <Mahbubbengali@gmail.com>
2025-07-02 15:40:23 +0600

    Add interactive Globe component and enhance home page layout. Integrated a new Globe component for visual representation of global data, including arcs and points. Updated the home page to feature the Globe, along with improved animations and content structure for a more engaging user experience. Adjusted styles for better responsiveness and visual appeal.

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Globe from "@/components/globe/Globe";
 import { motion } from "motion/react";
 
 const page = () => {
   return (
     <>
-      {/* <motion.div
+      <motion.div
         initial={{ opacity: 0.0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{
@@ -15,35 +16,47 @@ const page = () => {
         }}
         className="relative flex flex-col items-center justify-center gap-4 px-4"
       >
-        <div className="text-center text-3xl font-bold dark:text-white md:text-7xl">
+        <div className="mt-40 text-center text-3xl font-bold dark:text-white md:text-7xl">
           Background lights are cool you know.
         </div>
         <div className="py-4 text-base font-extralight dark:text-neutral-200 md:text-4xl">
           And this, is chemical burn.
         </div>
-        <button className="w-fit rounded-full bg-black px-4 py-2 text-white dark:bg-white dark:text-black">
+
+        <button className="mt-4 w-fit rounded-full bg-black px-4 py-2 text-white dark:bg-white dark:text-black">
           Debug now
         </button>
-      </motion.div> */}
-       <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
-      >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          Background lights are cool you know.
+
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            duration: 1,
+          }}
+          className="div"
+        >
+          <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
+            We sell products worldwide
+          </h2>
+          <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
+            This globe is interactive and customizable. Have fun with it, and
+            don&apos;t forget to share it. :)
+          </p>
+        </motion.div>
+
+        <div className="relative flex min-h-[400px] w-full items-center justify-center py-8 md:min-h-[600px]">
+          <div className="pointer-events-none absolute inset-x-0 bottom-0 z-40 h-40 w-full select-none bg-gradient-to-b from-transparent to-white dark:to-black" />
+          <div className="absolute z-10 h-full w-full">
+
+            <Globe />
+          </div>
         </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          And this, is chemical burn.
-        </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          Debug now
-        </button>
       </motion.div>
     </>
   );
