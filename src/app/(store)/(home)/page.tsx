dd20542e222ed506a0877aafe06d895279780cde
KaziMahbuburRahman <Mahbubbengali@gmail.com>
2025-07-03 10:08:56 +0600

    Add Button component with moving border effect and update page layout. Enhanced page.tsx by replacing a standard button with the new Button component, improving UI consistency. Updated package.json to include TypeScript watch command for development efficiency.

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Globe from "@/components/globe/Globe";
+import { Button } from "@/components/ui/moving-border";
 import { motion } from "motion/react";
 
 const page = () => {
@@ -23,9 +24,12 @@ const page = () => {
           And this, is chemical burn.
         </div>
 
-        <button className="mt-4 w-fit rounded-full bg-black px-4 py-2 text-white dark:bg-white dark:text-black">
+        <Button
+          borderRadius="1.75rem"
+          className="border-neutral-200 bg-white text-black dark:border-slate-800 dark:bg-slate-900 dark:text-white"
+        >
           Debug now
-        </button>
+        </Button>
 
         <motion.div
           initial={{
@@ -41,10 +45,10 @@ const page = () => {
           }}
           className="div"
         >
-          <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
+          <h2 className="text-center text-xl font-bold text-black dark:text-white md:text-4xl">
             We sell products worldwide
           </h2>
-          <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
+          <p className="mx-auto mt-2 max-w-md text-center text-base font-normal text-neutral-700 dark:text-neutral-200 md:text-lg">
             This globe is interactive and customizable. Have fun with it, and
             don&apos;t forget to share it. :)
           </p>
@@ -53,7 +57,6 @@ const page = () => {
         <div className="relative flex min-h-[400px] w-full items-center justify-center py-8 md:min-h-[600px]">
           <div className="pointer-events-none absolute inset-x-0 bottom-0 z-40 h-40 w-full select-none bg-gradient-to-b from-transparent to-white dark:to-black" />
           <div className="absolute z-10 h-full w-full">
-
             <Globe />
           </div>
         </div>
