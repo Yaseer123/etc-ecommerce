83a2e0516fe8bbd9239690e0cba7622cbc92e6c7
KaziMahbuburRahman <Mahbubbengali@gmail.com>
2025-07-03 09:52:06 +0600

    Remove unused authentication logic from layout component to streamline code. This change enhances maintainability by eliminating unnecessary imports.

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,7 +3,6 @@ import SlideNavbar from "@/components/shared/SlideNavbar";
 import Footer from "@/components/store-components/Footer";
 import ModalWrapper from "@/components/store-components/Modal/ModalWrapper";
 import WhatsAppWidget from "@/components/WhatsAppWidget";
-import { auth } from "@/server/auth";
 import "@/styles/styles.scss";
 import { HydrateClient } from "@/trpc/server";
 import React from "react";
@@ -13,8 +12,6 @@ export default async function layout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await auth();
-
   return (
     <HydrateClient>
       <SlideNavbar />
